d96765d7b68ff90d22f1a8f6d59de39238abf323
Laurens Deprost <ldeprost@gmail.com>
2020-09-07 18:42:36 +0200

    feat: Add logger

---
+++ src/log.ts
@@ -0,0 +1,34 @@
+import winston = require('winston');
+
+const getLogger = () => {
+  const alignColorsAndTime = winston.format.combine(
+    winston.format.colorize({
+      all: true,
+    }),
+    winston.format.label({
+      label: '[LOGGER]',
+    }),
+    winston.format.timestamp({
+      format: 'YY-MM-DD HH:mm:ss',
+    }),
+    winston.format.printf((info) => {
+      return `${info.label}  ${info.timestamp}  ${info.level} : ${info.message}`;
+    }),
+  );
+
+  const consoleTransport = new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format((info) => {
+        info.level = info.level.toUpperCase();
+        return info;
+      })(),
+      winston.format.colorize(),
+      alignColorsAndTime,
+    ),
+  });
+  return winston.createLogger({ transports: [consoleTransport] });
+};
+
+const log = getLogger();
+
+module.exports = log;
